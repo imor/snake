16069dfc25495e8811c77e53a61899beb58a8780
Raminder Singh <romi_ssk@yahoo.co.in>
2014-05-19 16:08:08 +0530

    Separate prototype for snake

--- js/game.js
+++ js/game.js
@@ -1,8 +1,6 @@
-var INITIAL_SNAKE_LENGTH = 3;
 var CELL_WIDTH = 10;
 var NUMBER_OF_GAME_CELLS = 40;
 var GAME_WIDTH = NUMBER_OF_GAME_CELLS * CELL_WIDTH;
-var INPUT_BUFFER_SIZE = 2;
 
 var DIRECTION = {
     UP: 1,
@@ -11,34 +9,21 @@ var DIRECTION = {
     LEFT: 4
 };
 
-var direction;
-var inputBuffer;
-var upArrowKey;
-var rightArrowKey;
-var downArrowKey;
-var leftArrowKey;
-var snakeCells;
+var snakes;
 var food;
-var snakeSpeed ;
 
 function init() {
-    direction = DIRECTION.RIGHT;
-    inputBuffer = [];
-    snakeCells = [];
-    food;
-    snakeSpeed = 5;
+    snakes = [];
+    game.state.start('main');
 }
 
 function restart() {
-    var length = snakeCells.length;
-    for (var i = 0;i < length;i++) {
-        snakeCells[i].kill();
-    }
+    snakes.forEach(function(snake) {
+        snake.kill();
+    });
     food.kill();
     init();
 }
-
-init();
 
 var game = new Phaser.Game(GAME_WIDTH, GAME_WIDTH, Phaser.AUTO, 'snake');
 
@@ -50,22 +35,9 @@ var main = {
     create: function() {
         game.stage.backgroundColor = '#ffffff';
 
-        for (var i = INITIAL_SNAKE_LENGTH - 1;i >= 0;--i) {
-            snakeCells.push(game.add.sprite(i * CELL_WIDTH, 0, 'cell'));
-        }
+        snakes.push(new Snake());
         var foodLocation = createFoodLocation();
         food = game.add.sprite(foodLocation.x, foodLocation.y, 'cell');
-
-        game.time.events.loop(Phaser.Timer.SECOND / snakeSpeed, moveSnake, this);
-
-        upArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        upArrowKey.onDown.add(onUpArrowKeyPressed, this);
-        rightArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        rightArrowKey.onDown.add(onRightArrowKeyPressed, this);
-        downArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-        downArrowKey.onDown.add(onDownArrowKeyPressed, this);
-        leftArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        leftArrowKey.onDown.add(onLeftArrowKeyPressed, this);
     },
 
     update: function() {
@@ -73,7 +45,35 @@ var main = {
 };
 
 game.state.add('main', main);
-game.state.start('main');
+init();
+
+function createFoodLocation() {
+    var x = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
+    var y = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
+    while (isSnakeLocation(x, y)) {
+        x = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
+        y = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
+    }
+
+    return {x:x, y:y};
+}
+
+function isFoodLocation(x, y) {
+    if (food && food.x === x && food.y === y) {
+        return true;
+    }
+    return false;
+}
+
+function isSnakeLocation(x, y) {
+    var result = false;
+    snakes.forEach(function(snake) {
+        if (snake.isSnakeLocation(x, y)) {
+            result = true;
+        }
+    });
+    return result;
+}
 
 function getAdjacentCellPosition(x, y, size, direction) {
     var newX;
@@ -105,112 +105,14 @@ function getAdjacentCellPosition(x, y, size, direction) {
     }
 }
 
-function moveSnake() {
-    var head = snakeCells[0];
-    var newDirection = inputBuffer.shift();
-    if (!newDirection) {
-        newDirection = direction;
-    }
-    var nextPosition = getAdjacentCellPosition(head.x, head.y, CELL_WIDTH, newDirection);
-    if (isFoodLocation(nextPosition.x, nextPosition.y)) {
-        snakeCells.unshift(game.add.sprite(nextPosition.x, nextPosition.y, 'cell'));
-        var foodLocation = createFoodLocation();
-        food.x = foodLocation.x;
-        food.y = foodLocation.y;
-    } else if (isInsideSnake(nextPosition.x, nextPosition.y)) {
-        restart();
-        game.state.start('main');
-        return;
-    }
-
-    var last = snakeCells.pop();
-    last.x = nextPosition.x;
-    last.y = nextPosition.y;
-    snakeCells.unshift(last);
-    direction = newDirection;
-}
-
-function createFoodLocation() {
-    var x = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
-    var y = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
-    while (isInsideSnake(x, y)) {
-        x = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
-        y = game.rnd.integerInRange(0, NUMBER_OF_GAME_CELLS - 1) * CELL_WIDTH;
-    }
-
-    return {x:x, y:y};
-}
-
-function isInsideSnake(x, y) {
-    var length = snakeCells.length;
-    for (var i = 0;i < length;i++) {
-        var cell = snakeCells[i];
-        if (cell.x === x && cell.y === y) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function isFoodLocation(x, y) {
-    if (food && food.x === x && food.y === y) {
-        return true;
-    }
-    return false;
-}
-
-function onUpArrowKeyPressed() {
-    var newDirection;
-    var oldDirection = direction;
-    if (inputBuffer.length > 0) {
-        oldDirection = inputBuffer[inputBuffer.length - 1];
-    }
-    if (oldDirection != DIRECTION.DOWN) {
-        newDirection = DIRECTION.UP;
-    }
-    if (newDirection && inputBuffer.length < INPUT_BUFFER_SIZE) {
-        inputBuffer.push(newDirection);
-    }
-}
-
-function onRightArrowKeyPressed() {
-    var newDirection;
-    var oldDirection = direction;
-    if (inputBuffer.length > 0) {
-        oldDirection = inputBuffer[inputBuffer.length - 1];
-    }
-    if (oldDirection != DIRECTION.LEFT) {
-        newDirection = DIRECTION.RIGHT;
-    }
-    if (newDirection && inputBuffer.length < INPUT_BUFFER_SIZE) {
-        inputBuffer.push(newDirection);
-    }
-}
-
-function onDownArrowKeyPressed() {
-    var newDirection;
-    var oldDirection = direction;
-    if (inputBuffer.length > 0) {
-        oldDirection = inputBuffer[inputBuffer.length - 1];
-    }
-    if (oldDirection != DIRECTION.UP) {
-        newDirection = DIRECTION.DOWN;
-    }
-    if (newDirection && inputBuffer.length < INPUT_BUFFER_SIZE) {
-        inputBuffer.push(newDirection);
-    }
-}
-
-function onLeftArrowKeyPressed() {
-    var newDirection;
-    var oldDirection = direction;
-    if (inputBuffer.length > 0) {
-        oldDirection = inputBuffer[inputBuffer.length - 1];
-    }
-    if (oldDirection != DIRECTION.RIGHT) {
-        newDirection = DIRECTION.LEFT;
-    }
-    if (newDirection && inputBuffer.length < INPUT_BUFFER_SIZE) {
-        inputBuffer.push(newDirection);
+function getOppositeDirection(direction) {
+    if (direction === DIRECTION.UP) {
+        return DIRECTION.DOWN;
+    } else if (direction === DIRECTION.RIGHT) {
+        return DIRECTION.LEFT;
+    } else if (direction === DIRECTION.DOWN) {
+        return DIRECTION.UP;
+    } else if (direction === DIRECTION.LEFT) {
+        return DIRECTION.RIGHT;
     }
 }
---
+++ js/snake.js
@@ -0,0 +1,96 @@
+var INITIAL_SNAKE_LENGTH = 3;
+var INPUT_BUFFER_SIZE = 2;
+
+function Snake() {
+    this.direction = DIRECTION.RIGHT;
+    this.inputBuffer = [];
+    this.snakeCells = [];
+    this.snakeSpeed = 5;
+    
+    for (var i = INITIAL_SNAKE_LENGTH - 1;i >= 0;--i) {
+        this.snakeCells.push(game.add.sprite(i * CELL_WIDTH, 0, 'cell'));
+    }
+    
+    Snake.prototype.move = function() {
+        var head = this.snakeCells[0];
+        var newDirection = this.inputBuffer.shift();
+        if (!newDirection) {
+            newDirection = this.direction;
+        }
+        var nextPosition = getAdjacentCellPosition(head.x, head.y, CELL_WIDTH, newDirection);
+        if (isFoodLocation(nextPosition.x, nextPosition.y)) {
+            this.snakeCells.unshift(game.add.sprite(nextPosition.x, nextPosition.y, 'cell'));
+            var foodLocation = createFoodLocation();
+            food.x = foodLocation.x;
+            food.y = foodLocation.y;
+        } else if (isSnakeLocation(nextPosition.x, nextPosition.y)) {
+            restart();
+            return;
+        }
+
+        var last = this.snakeCells.pop();
+        last.x = nextPosition.x;
+        last.y = nextPosition.y;
+        this.snakeCells.unshift(last);
+        this.direction = newDirection;
+    };
+    
+    Snake.prototype.handleKeyPressed = function(keyPressedDirection) {
+        var newDirection;
+        var oldDirection = this.direction;
+        if (this.inputBuffer.length > 0) {
+            oldDirection = this.inputBuffer[this.inputBuffer.length - 1];
+        }
+        if (oldDirection != getOppositeDirection(keyPressedDirection)) {
+            newDirection = keyPressedDirection;
+        }
+        if (newDirection && this.inputBuffer.length < INPUT_BUFFER_SIZE) {
+            this.inputBuffer.push(newDirection);
+        }
+    };
+
+    Snake.prototype.onUpArrowKeyPressed = function() {
+        this.handleKeyPressed(DIRECTION.UP);
+    };
+
+    Snake.prototype.onRightArrowKeyPressed = function () {
+        this.handleKeyPressed(DIRECTION.RIGHT);
+    };
+
+    Snake.prototype.onDownArrowKeyPressed = function() {
+        this.handleKeyPressed(DIRECTION.DOWN);
+    };
+
+    Snake.prototype.onLeftArrowKeyPressed = function() {
+        this.handleKeyPressed(DIRECTION.LEFT);
+    };
+    
+    Snake.prototype.isSnakeLocation = function(x, y) {
+        var length = this.snakeCells.length;
+        for (var i = 0;i < length;i++) {
+            var cell = this.snakeCells[i];
+            if (cell.x === x && cell.y === y) {
+                return true;
+            }
+        }
+        return false;
+    };
+    
+    Snake.prototype.kill = function() {
+        var length = this.snakeCells.length;
+        for (var i = 0;i < length;i++) {
+            this.snakeCells[i].kill();
+        }
+    };
+    
+    game.time.events.loop(Phaser.Timer.SECOND / this.snakeSpeed, this.move, this);
+
+    this.upArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    this.upArrowKey.onDown.add(this.onUpArrowKeyPressed, this);
+    this.rightArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    this.rightArrowKey.onDown.add(this.onRightArrowKeyPressed, this);
+    this.downArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    this.downArrowKey.onDown.add(this.onDownArrowKeyPressed, this);
+    this.leftArrowKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    this.leftArrowKey.onDown.add(this.onLeftArrowKeyPressed, this);
+}
